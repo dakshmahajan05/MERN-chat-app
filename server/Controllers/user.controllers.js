6a46efc2c7b849e4bfca795c662d3cb216d0f42f
Daksh Mahajan <mahajandaksh682@gmail.com>
2025-12-19 22:17:22 +0530

    created user model and basic login and signup controller

---
+++ server/Controllers/user.controllers.js
@@ -0,0 +1,65 @@
+import { genToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+import bcrypt from 'bcrypt'
+
+//sign up a new User
+export const signup=async(req,res)=>{
+    try {
+        const {fullName,email,password,bio} = req.body;
+        if(!fullName || !email || !password || !bio){
+            return res.status(400).json({message:"missing details",success:false})
+        }
+        const user = await User.findOne({email})
+        if(user){
+            return res.status(401).json({message:"accoubtn already exists",success:false})
+        }
+
+        const salt = await bcrypt.genSalt(10)
+        const hashPass = await bcrypt.hash(password,salt)
+
+        const newUser = await new User({
+            fullName,email,password:hashPass,bio
+        })
+
+        const token = genToken(newUser._id)
+
+        return res.status(200).json({message:"new user register successfull",user:newUser,token:token})
+
+    } catch (error) {
+        console.log(error);
+        
+        return res.json({message:error.message,success:false})
+
+    }
+}
+
+//controller for user login\
+
+export const login = async(req,res)=>{
+    try {
+        const {email,password} = req.body;
+        const user = await User.findOne({email})
+        if(!user){
+            return res.status(400).json({message:"user doesn't exist",success:false})
+        }
+
+        const ispassCorrect = await bcrypt.compare(password,user.password)
+        if(!ispassCorrect){
+            return res.status(401).json({message:"incorrect password",success:false})
+        }
+
+        const token = genToken(user._id)
+        return res.status(200).json({message:"login successfull",success:true})
+
+        
+    } catch (error) {
+        return res.status(400).json({message:"login faiiled",success:false})
+    }
+}
+
+
+//controller to check if user is authenticated or not
+
+export const checkAuth = (req,res)=>{
+    res.json({success:true,message:"user verified",user:req.user})
+}
---
+++ server/middlewares/auth.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken'
+
+
+//middleware to protect route 
+import User from "../models/user.model.js"
+
+export const protectRoute = async(req,res,next)=>{
+    try {
+        const token = req.headers.token;
+
+        const decoded=jwt.verify(token,process.env.JWT_SECRET)
+
+        const user  = await User.findById(decoded.userId).select("password")
+
+        if(!user){
+            return res.json({message:"cant find the user",sucess:false})
+        }
+
+        req.user = user;
+        next();
+
+
+    } catch (error) {
+        console.log(error.message);
+        return res.json({sucess:false,message:error.message})
+    }
+}
